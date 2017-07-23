7b3a7ee64a4fc7a678c798aeadabce983763b270
mavis <r03241304@ntu.edu.tw>
2017-07-23 08:39:34 +0800

    add column ranking and stock type

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, \
 	request, jsonify, render_template_string, Response,\
 	stream_with_context
 
+
 import os
 import json
 import urllib
@@ -36,29 +37,43 @@ def question():
 @app.route('/list_r', methods=['POST'])
 # @app.route('/list_r')
 def list_r():
+    # print (request.)
+    # data = request.get_data()
+    # json_data=json.loads(data)
+    # print (json_data)
+   
     try:
-        invest_score = request.json['invest_score']
-        industry_type = request.json['industry_type']
-        topics = request.json['topics']
-    except:
-        pass
-    invest_score = "PR1" #RR1,RR2,RR3
-    stock_ID = [2845,6005,1704,1727,2409,1216,1722,2856,1535]
-    industry_type = ["金融業", "食品工業", "化學工業"]
-    topics = ["供應商社會衝擊評估", "社會法規遵循", "社會公益／社會參與／社會影響"]
-    data_frame_topics, data_frame_risks = get_all_excel()
-    get_stock_select_industry = base_on_stock_ID_get_dataframe(data_frame_risks, stock_ID)
-    get_industry_frame = get_industry(get_stock_select_industry, industry_type)
+        data = request.get_data()
+        json_data=json.loads(data)
+        print (data)
+        invest_score = json_data.get('invest_score')
+        industry_type = json_data.get('industry_type')
+        topics = json_data.get('topics')
+        risks = json_data.get('risk')
+    except Exception as e:
+        print (e)
+        invest_score = "PR1" #RR1,RR2,RR3
 
-    get_stock_select_industry_risk = base_on_stock_ID_get_dataframe(data_frame_topics, stock_ID)
-    get_topics_frame = get_topics(get_stock_select_industry_risk, topics)
-    result_industrys = []
-    for index, row in get_topics_frame.iterrows():
+        industry_type = ["金融業", "食品工業", "化學工業"]
+        topics = ["經濟績效", "間接經濟衝擊", "市場形象/企業品牌"]
+        risks = ["商譽／企業形象", "市場風險"]
+    print (invest_score, industry_type, topics) 
+    stock_ID = [2845,6005,1704,1727,2409,1216,1722,2856,1535]    
+    data_frame_topics, data_frame_risks, data_frame_ranking = get_all_excel()
+    get_stock_select_industry_type = base_on_stock_ID_get_dataframe(data_frame_topics, stock_ID)
+    get_topics_frame = get_topics(get_stock_select_industry_type, topics)
+    get_risk_frame = get_risk(data_frame_risks, get_topics_frame, risks)
+    get_risk_frame = isnan_tozero(get_risk_frame)
+    get_ranking_frame = get_ranking(data_frame_ranking, get_risk_frame)
+    result_industrys = list()
+    for index, row in get_ranking_frame.iterrows():
         result_industry = {}
         result_industry.update({
-            'name':row['企業完整名稱'],
-            'industry_type':row['產業類別'],
-            'stock_id': row['股票代碼']
+            "name":row['企業完整名稱'],
+            "industry_type":row['產業類別'],
+            "stock_id": row['股票代碼'],
+            "PR": row['社會PR'],
+            "stock_type": row['股票類型']
         })
         result_industrys.append(result_industry)
     print (result_industrys)
@@ -66,7 +81,7 @@ def list_r():
 #     # return render_template('list.html')
 #     return render_template('list.html', data=result_industrys)
 
-    return render_template('list.html', data=result_industrys)
+    return render_template('list.html', data=json.dumps(result_industrys), mimetype='application/json')
     #Response(stream_template('list.html', data=result_industrys))
     # return Response(stream_with_context(json.dumps(result_industrys)))
 
@@ -151,12 +166,14 @@ def get_all_excel():
 #     data_frame = pd.read_excel('./2017hackntu/2016_CSR_Data_458_B_index.xlsx',error_bad_lines=False)
     data_frame_topics = pd.read_excel('./data/2016_CSR_Data_458_D_topics_new.xlsx')
     data_frame_risks = pd.read_excel('./data/2016_CSR_Data_458_B_risks.xlsx')
-    return data_frame_topics, data_frame_risks
+    data_frame_ranking = pd.read_excel('./data/2016_CSR_Data_458_Ranking.xlsx')
+    return data_frame_topics, data_frame_risks, data_frame_ranking
 
-def base_on_stock_ID_get_dataframe(data_frame_risks, stock_ID):
-    return data_frame_risks[data_frame_risks['股票代碼'].isin(stock_ID)]
+def base_on_stock_ID_get_dataframe(data_frame_topics, stock_ID):
+    return data_frame_topics[data_frame_topics['股票代碼'].isin(stock_ID)]
 
 def get_industry(stock_select_industry, industry_type):
+    print (get_stock_select_industry_type)
     return stock_select_industry[stock_select_industry['產業類別'].isin(industry_type)]
 
 def get_topics(industry_frame, topics):
@@ -166,6 +183,21 @@ def get_topics(industry_frame, topics):
         industry_frame_c.loc[industry_frame_c[industry_frame_c[topic].isin(['1'])].index, ['分數']] += 1
     return industry_frame_c.sort_values (['分數'], ascending=[False])
 
- 
+def get_risk(data_frame_risks, get_topics_frame, risks):
+    data_frame_risks_c = data_frame_risks.copy(deep=True)
+    data_frame_risks_c['分數'] = 0
+    is_in_get_topics_dataframe = get_topics_frame['產業類別']
+    data_frame_risks_c = data_frame_risks_c[data_frame_risks_c['產業類別'].isin(is_in_get_topics_dataframe)]
+    for risk in risks:
+        data_frame_risks_c.loc[data_frame_risks_c[data_frame_risks_c[risk].isin(['1'])].index, ['分數']] += 1
+    return data_frame_risks_c.sort_values (['分數'], ascending=[False])
+
+def get_ranking(data_frame_ranking, get_risk_frame):
+    is_in_get_risks_dataframe = get_risk_frame['股票代碼']
+    return data_frame_ranking[data_frame_ranking['股票代碼'].isin(is_in_get_risks_dataframe)]
+
+def isnan_tozero(get_risk_frame):
+    return get_risk_frame.fillna('no data') 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
